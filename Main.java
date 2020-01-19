ed0bd55eb10dc69549b386051c8aa202627a8c9a
diego59x <diegoalvarez1237@hotmail.com>
2020-01-19 17:29:28 -0600

    Se crearon las clases

---
+++ Main.java
@@ -0,0 +1,22 @@
+/*
+
+-------------------------------------------------
+Main.java
+-Oliver Truste 19
+-Diego Alvarez 19498
+
+Fecha de creacion: 19/01/2020
+-------------------------------------------------
+
+*/
+
+import java.util.Scanner;
+public class Main{
+	
+	public static void main (String[] args){
+		Scanner input = new Scanner(System.in);
+		Radio radio = new Radio();
+
+
+	}
+}
